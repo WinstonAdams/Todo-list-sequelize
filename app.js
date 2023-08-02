dac7602b9d8f61257b0c723b052b2de294264e16
WinstonAdams <apollo07291@gmail.com>
2023-08-02 22:55:45 +0800

    feat: add flash and modify register route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
-const bcrypt = require('bcryptjs')
+const flash = require('connect-flash')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -28,6 +28,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+
+  next()
+})
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -26,12 +26,25 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  // 若 errors 陣列中有元素時
+  if (errors.length) {
+    // errors 傳給 register，再傳給 message
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
 
   User.findOne({ where: { email } })
     .then(user => {
       if (user) {
-        console.log('User already exists')
+        errors.push({ message: '這個 Email 已經註冊過了。' })
         return res.render('register', {
+          errors,
           name,
           email,
           password,
