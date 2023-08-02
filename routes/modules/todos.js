f4440574b955cf635f5cba5c9b055286e473055e
WinstonAdams <apollo07291@gmail.com>
2023-08-02 23:22:18 +0800

    feat: add new function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,6 +6,19 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const name = req.body.name
+  const UserId = req.user.id
+
+  Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
 
