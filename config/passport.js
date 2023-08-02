c8af302fee8166cea0738ab693c57975225e6d9a
WinstonAdams <apollo07291@gmail.com>
2023-08-02 23:38:10 +0800

    feat: add error_msg

--- config/passport.js
+++ config/passport.js
@@ -12,23 +12,27 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
-        }
-        return bcrypt.compare(password, user.password)
-          .then(isMatch => {
-            if (!isMatch) {
-              return done(null, false, { message: 'Email or Password incorrect.' })
-            }
-            return done(null, user)
-          })
-          .catch(err => done(err, false))
-      })
-      .catch(err => done(err, false))
-  }))
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passReqToCallback: true
+  },
+    (req, email, password, done) => {
+      User.findOne({ where: { email } })
+        .then(user => {
+          if (!user) {
+            return done(null, false, req.flash('error_msg', 'That email is not registered!'))
+          }
+          return bcrypt.compare(password, user.password)
+            .then(isMatch => {
+              if (!isMatch) {
+                return done(null, false, req.flash('error_msg', 'Password incorrect.'))
+              }
+              return done(null, user)
+            })
+            .catch(err => done(err, false))
+        })
+        .catch(err => done(err, false))
+    }))
 
   passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_ID,
